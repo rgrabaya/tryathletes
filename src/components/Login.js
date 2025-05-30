73180c17f66c0fc9d61d1823f86b0ff2e80860fb
RG Rabaya <rgrabaya@gmail.com>
2025-05-30 22:39:54 +0800

    install bulmba, add login page

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+  const clientId = '162236';
+
+  return (
+    <>
+      <h2 className="is-size-3 mb-4">Welcome to Try Athletes Club</h2>
+      <button className="button">Login with Strava {clientId}</button>
+    </>
+  );
+};
+
+export default Login;
